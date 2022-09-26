0d5eeae54750590148d40d07bf6313a51a49b7f8
Hunter <Hunter Rabun>
2022-09-26 17:12:28 -0400

    Added command to get rankings by number

--- Client.py
+++ Client.py
@@ -1,5 +1,7 @@
 import discord
-from private.config import Token
+from discord import app_commands
+from private.config import DiscordToken
+from Commands import callbacks
 
 
 class Client(discord.Client):
@@ -11,11 +13,18 @@ class Client(discord.Client):
 
     async def on_ready(self):
         print("We have logged in as {0}".format(self.user))
+        await tree.sync(guild=discord.Object(id=1018192683723915284))
+
 
 
 def main():
     client = Client()
-    client.run(Token)
+    global tree
+    tree = app_commands.CommandTree(client)
+    callbacks(tree)
+
+    client.run(DiscordToken)
 
 if __name__ == "__main__":
     main()
+
--- Commands.py
+++ Commands.py
@@ -14,10 +14,20 @@ def callbacks(tree : app_commands.CommandTree):
     async def self(interaction: discord.Interaction):
         await interaction.response.send_message(f"Hello {interaction.user.name}!")
 
-    @tree.command(name = "cfb_rank", description="Prints the current CFB rankings", guild = discord.Object(id=1018192683723915284))
+    @tree.command(name = "cfb_rank", description="Prints the current AP Top 25 CFB rankings", guild = discord.Object(id=1018192683723915284))
     async def self(interaction : discord.Interaction):
         r = cfb_obj.get_rankings()
         index = 0
         for schools in r:
+            await interaction.channel.send(f"Rank #{index + 1} {schools}")
+            index += 1
+
+    @tree.command(name = "cfb_rank_no", description="Prints the current AP Top 25 CFB rankings based on the number given", guild = discord.Object(id=1018192683723915284))
+    async def self(interaction : discord.Interaction, number : str):
+        r = cfb_obj.get_rankings()
+        index = 0
+        for schools in r:
+            if index + 1 > int(number):
+                break 
             await interaction.channel.send(f"Rank #{index + 1} {schools}")
             index += 1
